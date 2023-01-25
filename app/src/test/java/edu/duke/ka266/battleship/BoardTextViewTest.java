45758360f01f5a4fe7d3839284aead2b091dca6d
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2023-01-25 10:24:44 -0500

    task8.txt

--- app/src/test/java/edu/duke/ka266/battleship/BoardTextViewTest.java
+++ app/src/test/java/edu/duke/ka266/battleship/BoardTextViewTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTextViewTest {
 
-  private void emptyBoardHelper(int w, int h, String expectedHeader, String body) {
-    Board<Character> b1 = new BattleShipBoard<Character>(w, h);
+  private void emptyBoardHelper(Board<Character> b1, String expectedHeader, String body) {
     BoardTextView view = new BoardTextView(b1);
     assertEquals(expectedHeader, view.makeHeader());
     String expected = expectedHeader + body + expectedHeader;
@@ -16,22 +15,28 @@ public class BoardTextViewTest {
 
   @Test
   public void test_display_empty_2by2() {
+    Board<Character> b1 = new BattleShipBoard<Character>(2, 2);
+
     String expectedHeader = "  0|1\n";
     String body = "A  |  A\n" +
         "B  |  B\n";
-    emptyBoardHelper(2, 2, expectedHeader, body);
+    emptyBoardHelper(b1, expectedHeader, body);
   }
 
   @Test
   public void test_display_empty_3by2() {
+    Board<Character> b1 = new BattleShipBoard<Character>(3, 2);
+
     String expectedHeader = "  0|1|2\n";
     String body = "A  | |  A\n" +
         "B  | |  B\n";
-    emptyBoardHelper(3, 2, expectedHeader, body);
+    emptyBoardHelper(b1, expectedHeader, body);
   }
 
   @Test
   public void test_display_empty_3by5() {
+    Board<Character> b1 = new BattleShipBoard<Character>(3, 5);
+
     String expectedHeader = "  0|1|2\n";
     String body = "A  | |  A\n" +
         "B  | |  B\n" +
@@ -39,7 +44,40 @@ public class BoardTextViewTest {
         "D  | |  D\n" +
         "E  | |  E\n";
 
-    emptyBoardHelper(3, 5, expectedHeader, body);
+    emptyBoardHelper(b1, expectedHeader, body);
+  }
+
+  @Test
+  public void test_display_2by2() {
+    Board<Character> b1 = new BattleShipBoard<Character>(2, 2);
+    BasicShip s1 = new BasicShip(new Coordinate(1, 1));
+    b1.tryAddShip(s1);
+
+    String expectedHeader = "  0|1\n";
+    String body = "A  |  A\n" +
+        "B  |s B\n";
+    emptyBoardHelper(b1, expectedHeader, body);
+  }
+
+  @Test
+  public void test_display_3by5() {
+    Board<Character> b1 = new BattleShipBoard<Character>(3, 5);
+    BasicShip s1 = new BasicShip(new Coordinate(1, 1));
+    b1.tryAddShip(s1);
+    BasicShip s2 = new BasicShip(new Coordinate(3, 0));
+    b1.tryAddShip(s2);
+     BasicShip s3 = new BasicShip(new Coordinate(4, 2));
+    b1.tryAddShip(s3);
+
+
+    String expectedHeader = "  0|1|2\n";
+    String body = "A  | |  A\n" +
+        "B  |s|  B\n" +
+        "C  | |  C\n" +
+        "D s| |  D\n" +
+        "E  | |s E\n";
+
+    emptyBoardHelper(b1, expectedHeader, body);
   }
 
   @Test
